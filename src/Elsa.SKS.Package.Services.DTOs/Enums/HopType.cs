9d6510e41438c00206f9a49e4ae0e9d2688af55d
expries <56935464+expries@users.noreply.github.com>
2021-09-23 12:15:12 +0200

    Added EnumMember to HopType enum

--- src/Elsa.SKS.Package.Services.DTOs/Enums/HopType.cs
+++ src/Elsa.SKS.Package.Services.DTOs/Enums/HopType.cs
@@ -1,10 +1,16 @@
-﻿namespace Elsa.SKS.Package.Services.DTOs.Enums
+﻿using System.Runtime.Serialization;
+
+namespace Elsa.SKS.Package.Services.DTOs.Enums
 {
     public enum HopType
     {
+        [EnumMember(Value = "None")]
         None,
+        [EnumMember(Value = "Warehouse")]
         Warehouse,
+        [EnumMember(Value = "Truck")]
         Truck,
+        [EnumMember(Value = "TransferWarehouse")]
         TransferWarehouse
     }
 }
