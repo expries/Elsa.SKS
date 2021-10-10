adf2dcc209c88bdd4b9693c708d1f7de04cfdf04
pixelini <eglatz@gmx.at>
2021-10-10 14:46:43 +0200

    added validations for 4a- 4d

---
+++ src/Elsa.SKS.Package.BusinessLogic/Validators/AddressValidator.cs
@@ -0,0 +1,18 @@
+﻿using Elsa.SKS.Package.BusinessLogic.Entities;
+using FluentValidation;
+
+namespace Elsa.SKS.Package.BusinessLogic.Validators
+{
+    public class AddressValidator : AbstractValidator<User>
+    {
+        public AddressValidator()
+        {
+            RuleFor(address => address.Country)
+                .Must(country => country is "Austria" or "Österreich");
+
+            RuleFor(address => address.PostalCode).Matches("(^A-[0-9]{4}$)");
+            RuleFor(address => address.Street).Matches("(^([A-Za-zß]+ )*[0-9\\w\\/]*)");
+            RuleFor(address => address.City).Matches("(^[A-Z]{1}[A-Za-z\\-\\ ]*$)");
+        }
+    }
+}
---
+++ src/Elsa.SKS.Package.BusinessLogic/Validators/HopArrivalValidator.cs
@@ -0,0 +1,14 @@
+﻿using Elsa.SKS.Package.BusinessLogic.Entities;
+using FluentValidation;
+
+namespace Elsa.SKS.Package.BusinessLogic.Validators
+{
+    public class HopArrivalValidator : AbstractValidator<HopArrival>
+    {
+        public HopArrivalValidator()
+        {
+            RuleFor(hopArrival => hopArrival.Hop.Code).Matches("(^[A-Z]{4}\\d{1,4}$");
+        }
+
+    }
+}
