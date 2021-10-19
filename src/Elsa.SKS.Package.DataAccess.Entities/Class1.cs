32b0a59c0e4e3518aae4cde25f39e15350fdef1b
pixelini <eglatz@gmx.at>
2021-10-19 20:34:29 +0200

    added projects for DAL

---
+++ src/Elsa.SKS.Package.DataAccess.Entities/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Elsa.SKS.Package.DataAccess.Entities
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/Elsa.SKS.Package.DataAccess.Sql/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Elsa.SKS.Package.DataAccess.Sql
+{
+    public class Class1
+    {
+    }
+}
