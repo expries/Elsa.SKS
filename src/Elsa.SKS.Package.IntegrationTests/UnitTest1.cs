5226656949ea860ebc68c50eb542da8d5eb1eae7
expries <56935464+expries@users.noreply.github.com>
2021-12-10 16:19:14 +0100

    Move Webhooks.DTO project into src folder

---
+++ src/Elsa.SKS.Package.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace Elsa.SKS.Package.IntegrationTests
+{
+    public class WarehouseApiTests
+    {
+        [Fact]
+        public void TestImportWarehouseHierarchy()
+        {
+
+        }
+    }
+}
+
